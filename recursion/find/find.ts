74438def275ce97da29b0142538022ca57e171b6
Matt Smida <mcs@m4t7.com>
2024-02-27 18:31:17 -0500

    implemented find()

--- recursion/find/find.ts
+++ recursion/find/find.ts
@@ -1,7 +1,13 @@
 /** find: return t/f is val is in arr. */
 
 function find(arr: number[], val: number): boolean {
-  return true;
+  // Keep calling the find function until we're at the end of arr
+  // Base case will be [], which returns False
+
+  if (arr.length === 0) return false;
+
+  if (arr[0] === val) return true;
+  return find(arr.slice(1), val);
 }
 
 export { find };
